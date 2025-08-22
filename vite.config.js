eebf9340a40dfda0625e4a3e7bc503e7fe43b555
selvaganapathi-DA <selvamecsoft@gmail.com>
2025-08-23 00:54:32 +0530

    second update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://selvaganapathi-da.github.io/CHIT_TRS/',
 })
